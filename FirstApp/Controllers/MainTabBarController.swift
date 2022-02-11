3d86dc3a05a4a10874b73fe0a8ec26260ef05761
MacLex <>
2022-02-11 14:12:24 +0300

    update

--- FirstApp/Controllers/MainTabBarController.swift
+++ FirstApp/Controllers/MainTabBarController.swift
@@ -14,7 +14,6 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         setupTapBar()
         setupItems()
         
